14adc12bd0cbc4c4366352a9e916d55cc5f4a495
maitzeth <andre801mz@gmail.com>
2024-04-04 01:17:02 -0300

    feat: :sparkles: adding drink category buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,6 +31,41 @@ export default function Home({ products }: HomeProps) {
         </div>
         <SearchInput placeholder="Search burger, pizza, drink or ect..." />
         <DividerTitle title="Drink Category" href="/drinks?category=all" />
+        <div className="flex space-x-2 w-full">
+          <ButtonQuaternary isSelected={false} onClick={() => console.log('zzz')}>
+            All
+          </ButtonQuaternary> 
+          <ButtonQuaternary
+            isFlexFull
+            isSelected
+            onClick={() => console.log('zzz')}
+          >
+            <div className="flex space-x-3 items-center -ml-10">
+              <div>
+                <Image
+                  src="/images/beer.png"
+                  height={20}
+                  width={20}
+                  alt="Beer Icon"
+                />
+              </div>
+              <span>Beer</span>
+            </div>
+          </ButtonQuaternary> 
+          <ButtonQuaternary isFlexFull onClick={() => console.log('zzz')}>
+            <div className="flex space-x-3 items-center -ml-10">
+              <div>
+                <Image
+                  src="/images/wine-glass.png"
+                  height={20}
+                  width={20}
+                  alt="Wine Icon"
+                />
+              </div>
+              <span>Wine</span>
+            </div>
+          </ButtonQuaternary>
+        </div>
         <DividerTitle title="Popular" href="/drinks?popular=all" />
       </header>
     </DefaultLayout>
