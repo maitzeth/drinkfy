8e617c2d457822378d1b6fbe3791e144c5848d38
maitzeth <andre801mz@gmail.com>
2024-04-05 01:43:26 -0300

    fix: fixing stock data to JSON

--- src/pages/api/stock-price/[id].ts
+++ src/pages/api/stock-price/[id].ts
@@ -9,18 +9,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { id } = req.query as ReqData;
-  const stock = await getStockPriceById(id);
-  res.json(JSON.parse(JSON.stringify(stock)))
-  // try {
-  //   res.status(200).json(stock);
-  // } catch (err) {
-  //   const error = err as Error;
+  try {
+    const { id } = req.query as ReqData;
+    const stock = await getStockPriceById(id);
+    res.json(JSON.parse(JSON.stringify(stock)))
+  } catch (err) {
+    const error = err as Error;
 
-  //   res.status(400).json({
-  //     stock: 0,
-  //     price: 0,
-  //     error: error.message,
-  //   });
-  // }
+    res.status(400).json({
+      stock: 0,
+      price: 0,
+      error: error.message,
+    });
+  }
 }
